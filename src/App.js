22f88fccef0e780a5e62b0913a07ab0a7825f1ad
Thiago <tlcrezende@gmail.com>
2023-02-07 11:12:36 -0300

    Add accordion and remove button from app

--- src/App.js
+++ src/App.js
@@ -1,41 +1,26 @@
-import Button from "./components/Button";
-import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go'
+import Accordion from "./components/Accordion";
 
 
 function App() {
+  const items = [
+    {
+      id: 1,
+      label: "oi",
+      content: "content"
+    },
+    {
+      id: 2,
+      label: "oi",
+      content: "content"
+    },
+    {
+      id: 3,
+      label: "oi",
+      content: "content"
+    }
+  ]
 
-  return (
-    <div>
-      <div>
-        <Button primary rounded>
-          <GoBell/>
-          oioioi
-        </Button>
-      </div>
-      <div>
-        <Button secondary rounded>
-          <GoCloudDownload/>
-          oioioi
-        </Button>
-      </div>
-      <div>
-        <Button warning rounded>
-          <GoDatabase/>
-          oioioi
-        </Button>
-      </div>
-      <div>
-        <Button danger rounded>
-          oioioi
-        </Button>
-      </div>
-      <div>
-        <Button success rounded>
-          oioioi
-        </Button>
-      </div>
-    </div>
-  );
+  return <Accordion items={items}/>  
 }
 
 export default App;
