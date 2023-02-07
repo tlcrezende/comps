b3206a9b059c4901820c8903383a51e3f09215e4
Thiago <tlcrezende@gmail.com>
2023-02-07 11:43:30 -0300

    add accordion logic

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,22 +1,32 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 function Accordion({ items }) {
+  const [expandedIndex, setExpandedIndex] = useState(-1);
 
-  const [expandedIndex, setExpandedIndex] = useState(0);
-
-  
-  
   const renderedItems = items.map((item, index) => {
-    const isExpanded = index === expandedIndex
+    const isExpanded = index === expandedIndex;
+
+    const icon = (
+      <span className="text-2xl">{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+    );
 
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
-        <div>{item.content}</div>
+        <div
+          className="flex p-3 bg-gray-50 border-b items-center cursor-pointer"
+          onClick={() => {
+            setExpandedIndex(index);
+          }}
+        >
+          {icon}
+          {item.label}
+        </div>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
       </div>
     );
   });
-  return <div>{renderedItems}</div>;
+  return <div className="border-x border-t rounded">{renderedItems}</div>;
 }
 
 export default Accordion;
